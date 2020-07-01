06a6caa671afc1627aefb931fb42b1a542c67333
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2020-07-01 18:18:53 +0900

    Update init_param.py

--- init_param.py
+++ init_param.py
@@ -12,8 +12,8 @@ import glob, os
 
 
 # ----- Raw images ----- #
-dir_img = 'Images/'
-img_name = glob.glob(dir_img+'*.fits')
+dir_img = 'Images/'    # Raw data directory
+img_name = glob.glob(dir_img+'*.fits')    # Names of raw images
 
 
 # # ----- Cosmic ray masking parameters ----- #
@@ -22,24 +22,26 @@ img_name = glob.glob(dir_img+'*.fits')
 
 
 # ----- Initial photometric parameters ----- #
-pixscl0 = 0.05    # arcsec/pix (initial)
-pixscl1 = 0.05    # arcsec/pix (final)
-zmag = 25.0
-gain = 2.0
-fwhm = 2.0
-r_ap1, r_ap2 = 3.0, 6.0
-detect_thresh = 1.5
+zmag = 25.0    # Magnitude zeropoint
+gain = 2.0    # GAIN value
+fwhm = 2.0    # FWHM of point sources
+r_ap1, r_ap2 = 3.0, 6.0    # Radii of apertures for photometry
+detect_thresh = 1.5    # Detection threshold (sigma)
+tolerance = 1.5    # Matching tolerance & search radius (arcsec)
 
 
-# ----- Parameter cut ----- #
-err_cut = 0.30    # magnitude error cut
-cidx_lo_cut = 0.05    # lower C index cut
-cidx_hi_cut = 0.60    # higher C index cut
-mag_lo_cut = 17.0    # brighter magnitude cut
-mag_hi_cut = 25.0    # fainter magnitude cut
+# ----- Parameter cut (these should be revised interactively!) ----- #
+err_cut = 0.30    # Magnitude error cut
+cidx_lo_cut = 0.05    # Lower C index cut
+cidx_hi_cut = 0.60    # Higher C index cut
+mag_lo_cut = 17.0    # Rrighter magnitude cut
+mag_hi_cut = 25.0    # Fainter magnitude cut
 
 
 # ----- Initial setting for running drizzlepac ----- #
-ref_flt = '850'
-dir_twk = 'tweak/'
-dir_out = 'Results/'
+ref_flt = '850'    # Reference filter
+pixscl1 = 0.05    # arcsec/pix (final)
+dir_twk = 'tweak/'    # The directory for running TWEAKREG
+dir_out = 'Results/'    # Output image directory
+
+
