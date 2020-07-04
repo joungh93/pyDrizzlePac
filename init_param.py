9f34461810c0b69525bbfe37afdef0c71155b213
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2020-07-05 00:09:50 +0900

    Update init_param.py

--- init_param.py
+++ init_param.py
@@ -22,18 +22,17 @@ img_name = glob.glob(dir_img+'*.fits')    # Names of raw images
 
 
 # ----- Initial photometric parameters ----- #
-fwhm = 2.0    # FWHM of point sources
 r_ap1, r_ap2 = 3.0, 6.0    # Radii of apertures for photometry
 detect_thresh = 1.5    # Detection threshold (sigma)
 tolerance = 1.5    # Matching tolerance & search radius (arcsec)
 
 
 # ----- Parameter cut (these should be revised interactively!) ----- #
-err_cut = 0.30    # Magnitude error cut
+err_cut = 0.40    # Magnitude error cut
 cidx_lo_cut = 0.05    # Lower C index cut
 cidx_hi_cut = 0.60    # Higher C index cut
 mag_lo_cut = 17.0    # Rrighter magnitude cut
-mag_hi_cut = 25.0    # Fainter magnitude cut
+mag_hi_cut = 26.0    # Fainter magnitude cut
 
 
 # ----- Initial setting for running drizzlepac ----- #
@@ -41,5 +40,5 @@ ref_flt = '814'    # Reference filter
 pixscl1 = 0.05    # arcsec/pix (final)
 dir_twk = 'tweak/'    # The directory for running TWEAKREG
 dir_out = 'Results/'    # Output image directory
-
+n_minobj = 15    # Minimum number of objects for running TWEAKREG task
 
